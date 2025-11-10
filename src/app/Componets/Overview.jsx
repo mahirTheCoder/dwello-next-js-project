e94158f8e402bf4e5f41ecc5b25ed8321ae20dd9
mahirTheCoder <mahirchowdhury518@gmail.com>
2025-11-10 20:50:14 +0600

    responsive complete

--- src/app/Componets/Overview.jsx
+++ src/app/Componets/Overview.jsx
@@ -14,7 +14,7 @@ const Overview = () => {
               <h2 className="text-3xl font-semibold text-[#09231E] font-inter mb-4">
                 Project Overview
               </h2>
-              <p className="w-[872px] text-base font-normal text-[#4A5565] font-inter mb-12">
+              <p className="lg:w-[872px] w-80 lg:text-base text-xs font-normal text-[#4A5565] font-inter mb-12">
                 A luxury residential complex featuring eco-friendly design and
                 state-of-the-art amenities. Green Valley Residences combines
                 sustainable living with modern comfort, offering residents a
@@ -29,25 +29,25 @@ const Overview = () => {
                   </h2>
                   <div className="iconText flex gap-2 items-center mb-3">
                     <IoCheckmarkDoneCircleOutline className="text-[#2E7D32]  text-base " />
-                    <p className=" text-base font-normal text-[#4A5565] font-inter ">
+                    <p className=" lg:text-base text-sm font-normal text-[#4A5565] font-inter ">
                       Eco-friendly materials and construction methods
                     </p>
                   </div>
                   <div className="iconText flex gap-2 items-center mb-3">
                     <IoCheckmarkDoneCircleOutline className="text-[#2E7D32]  text-base " />
-                    <p className=" text-base font-normal text-[#4A5565] font-inter ">
+                    <p className="  lg:text-base text-sm font-normal text-[#4A5565] font-inter ">
                       Rainwater harvesting system
                     </p>
                   </div>
                   <div className="iconText flex gap-2 items-center mb-3">
                     <IoCheckmarkDoneCircleOutline className="text-[#2E7D32]  text-base " />
-                    <p className=" text-base font-normal text-[#4A5565] font-inter ">
+                    <p className="  lg:text-base text-sm font-normal text-[#4A5565] font-inter ">
                       Energy-efficient HVAC systems
                     </p>
                   </div>
                   <div className="iconText flex gap-2 items-center mb-3">
                     <IoCheckmarkDoneCircleOutline className="text-[#2E7D32]  text-base " />
-                    <p className=" text-base font-normal text-[#4A5565] font-inter ">
+                    <p className="  lg:text-base text-sm font-normal text-[#4A5565] font-inter ">
                       Modern fitness center and swimming pool
                     </p>
                   </div>
@@ -58,25 +58,25 @@ const Overview = () => {
                   </h2>
                   <div className="iconText flex gap-2 items-center mb-3">
                     <IoCheckmarkDoneCircleOutline className="text-[#2E7D32]  text-base " />
-                    <p className=" text-base font-normal text-[#4A5565] font-inter ">
+                    <p className="  lg:text-base text-sm font-normal text-[#4A5565] font-inter ">
                       Eco-friendly materials and construction methods
                     </p>
                   </div>
                   <div className="iconText flex gap-2 items-center mb-3">
                     <IoCheckmarkDoneCircleOutline className="text-[#2E7D32]  text-base " />
-                    <p className=" text-base font-normal text-[#4A5565] font-inter ">
+                    <p className="  lg:text-base text-sm font-normal text-[#4A5565] font-inter ">
                       Rainwater harvesting system
                     </p>
                   </div>
                   <div className="iconText flex gap-2 items-center mb-3">
                     <IoCheckmarkDoneCircleOutline className="text-[#2E7D32]  text-base " />
-                    <p className=" text-base font-normal text-[#4A5565] font-inter ">
+                    <p className="  lg:text-base text-sm font-normal text-[#4A5565] font-inter ">
                       Energy-efficient HVAC systems
                     </p>
                   </div>
                   <div className="iconText flex gap-2 items-center mb-3">
                     <IoCheckmarkDoneCircleOutline className="text-[#2E7D32]  text-base " />
-                    <p className=" text-base font-normal text-[#4A5565] font-inter ">
+                    <p className="  lg:text-base text-sm font-normal text-[#4A5565] font-inter ">
                       Modern fitness center and swimming pool
                     </p>
                   </div>
@@ -88,26 +88,26 @@ const Overview = () => {
                 <h2 className="text-xl font-semibold font-inter text-[#09231E] mb-5">
                    Project Challenges & Solutions
                   </h2>
-                <div className="content w-[872px] py-4 mb-4 bg-[#F9FAFB]">
+                <div className="content lg:w-[872px] w-80 py-4 mb-4 bg-[#F9FAFB]">
                   <div className="iconText flex gap-2 items-center pl-4 mb-3">
                     <PiWarningCircleBold  className="text-[#2E7D32]  text-base " />
-                    <p className=" text-base font-normal text-[#4A5565] font-inter ">
+                    <p className="  lg:text-base text-sm w-80 font-normal text-[#4A5565] font-inter ">
                      Implemented sustainable building practices while maintaining budget
                     </p>
                   </div>
                 </div>
-                <div className="content w-[872px] py-4 mb-4 bg-[#F9FAFB]">
+                <div className="content lg:w-[872px] w-80  py-4 mb-4 bg-[#F9FAFB]">
                   <div className="iconText flex gap-2 items-center pl-4 mb-3">
                     <PiWarningCircleBold  className="text-[#2E7D32]  text-base " />
-                    <p className=" text-base font-normal text-[#4A5565] font-inter ">
+                    <p className="  lg:text-base text-sm font-normal text-[#4A5565] font-inter ">
                      Implemented sustainable building practices while maintaining budget
                     </p>
                   </div>
                 </div>
-                <div className="content w-[872px] py-4 mb-4 bg-[#F9FAFB]">
+                <div className="content lg:w-[872px] w-80  py-4 mb-4 bg-[#F9FAFB]">
                   <div className="iconText flex gap-2 items-center pl-4 mb-3">
                     <PiWarningCircleBold  className="text-[#2E7D32]  text-base " />
-                    <p className=" text-base font-normal text-[#4A5565] font-inter ">
+                    <p className="  lg:text-base text-sm font-normal text-[#4A5565] font-inter ">
                      Implemented sustainable building practices while maintaining budget
                     </p>
                   </div>
