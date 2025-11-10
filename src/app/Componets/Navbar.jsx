a9dde8911dc62324a60179c0061e1e1462294741
mahirTheCoder <mahirchowdhury518@gmail.com>
2025-11-10 09:17:07 +0600

    image complete

--- src/app/Componets/Navbar.jsx
+++ src/app/Componets/Navbar.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
-
+import logo from '../../app/Images/logo (1).png'
+import Image from 'next/image'
 const Navbar = () => {
   return (
     <>
-    <div className="container">lorem</div>
+    <section id='navbar'>
+<div className="container">
+    <div className="navItems">
+        <div className="img-part">
+            <Image src={logo} alt='logo' width={93} height={29} />
+        </div>
+        <div className="menuItems">
+            
+        </div>
+        <div className="search-login-button"></div>
+    </div>
+</div>
+    </section>
     </>
   )
 }
