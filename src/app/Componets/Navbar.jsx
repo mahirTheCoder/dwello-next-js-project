18c9340efcce6ee02f861c99ae1b0f96f118bf22
mahirTheCoder <mahirchowdhury518@gmail.com>
2025-11-10 09:34:07 +0600

    menu item complete

--- src/app/Componets/Navbar.jsx
+++ src/app/Componets/Navbar.jsx
@@ -10,8 +10,11 @@ const Navbar = () => {
         <div className="img-part">
             <Image src={logo} alt='logo' width={93} height={29} />
         </div>
-        <div className="menuItems">
-            
+        <div className="menuItems flex gap-10">
+            <a href="#" className='text-main text-lg font-bold font-poppins text '>Home</a>
+            <a href="#" className='text-main text-lg font-bold font-poppins text '>Service</a>
+            <a href="#" className='text-main text-lg font-bold font-poppins text '>Agents</a>
+            <a href="#" className='text-main text-lg font-bold font-poppins text '>Contact</a>
         </div>
         <div className="search-login-button"></div>
     </div>
